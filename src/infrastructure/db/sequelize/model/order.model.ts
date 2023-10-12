7393bd2ae2f5771065c2ffdf59f605d12dcb8cd3
Ricardo Gonçalves <ricardo@conceitho.com>
2023-10-12 00:27:56 -0300

    feat: adiciona modelos para repositórios de pedidos

---
+++ src/infrastructure/db/sequelize/model/order.model.ts
@@ -0,0 +1,29 @@
+import { BelongsTo, Column, ForeignKey, HasMany, Model, PrimaryKey, Table } from "sequelize-typescript";
+import { CustomerModel } from "./customer.model";
+import { Customer } from "../../../../domain/entity/customer";
+import { OrderItemModel } from "./order-item.model";
+
+@Table(
+    {
+        tableName: "orders",
+        timestamps: false,
+    })
+export class OrderModel extends Model {
+    @PrimaryKey
+    @Column
+    declare id: string;
+
+    @ForeignKey(() => CustomerModel)
+    @Column({ allowNull: false })
+    declare customer_id: string;
+
+
+    @BelongsTo(() => CustomerModel)
+    declare customer: CustomerModel;
+
+    @HasMany(() => OrderItemModel)
+    declare items: OrderItemModel[];
+
+    @Column({ allowNull: false, defaultValue: 0 })
+    declare total: number;
+}
