5a1428487f0987473ec9f78d53bfdd2591e3b7cc
Ricardo Gonçalves <ricardo@conceitho.com>
2023-10-12 15:10:08 -0300

    feat: adiciona lista de handlers ao dispatcher

--- src/domain/event/@shared/event-dispatcher.interface.ts
+++ src/domain/event/@shared/event-dispatcher.interface.ts
@@ -1,7 +1,9 @@
-import { IEventHandler } from "./event-handler.interface";
-import { IEvent } from "./event.interface";
+import { IEvent } from './event.interface';
+import { IEventHandler } from './event-handler.interface';
 
 export interface IEventDispatcher {
+    handlers: { [eventName: string]: IEventHandler[] };
+
     notify(event: IEvent): void;
     register(eventName: string, handler: IEventHandler): void;
     unregister(eventName: string, handler: IEventHandler): void;
