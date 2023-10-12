9f42d3e7260d175998a77844b03ca4de929dd2c5
Ricardo Gonçalves <ricardo@conceitho.com>
2023-10-12 19:03:47 -0300

    test: adiciona teste de eventos de domínio ao Customer

--- src/domain/customer/entity/customer.spec.ts
+++ src/domain/customer/entity/customer.spec.ts
@@ -1,6 +1,10 @@
-import { Address } from "../value-object/address";
-import { Customer } from "./customer";
-import { v4 as uuid } from "uuid";
+import { newCustomer } from '../../../infrastructure/@shared/repository/repository.utils';
+import { SendConsoleLogWhenCustomerAddressIsChangedHandler } from '../event/handler/send-console-log-when-customer-address-is-changed.handler';
+import { SendConsoleLogWhenCustomerIsCreatedHandler1 } from '../event/handler/send-console-log-when-customer-is-created1.handler';
+import { SendConsoleLogWhenCustomerIsCreatedHandler2 } from '../event/handler/send-console-log-when-customer-is-created2.handler copy';
+import { Address } from '../value-object/address';
+import { Customer } from './customer';
+import { v4 as uuid } from 'uuid';
 
 describe("Customer unit tests", () => {
     it("should throw error when id is empty", () => {
@@ -59,5 +63,34 @@ describe("Customer unit tests", () => {
 
         customer.addRewardsPoints(20);
         expect(customer.rewardsPoints).toBe(30);
+    });
+
+    it("Should trigger event when created customer", () => {
+        const h1 = new SendConsoleLogWhenCustomerIsCreatedHandler1();
+        const h2 = new SendConsoleLogWhenCustomerIsCreatedHandler2();
+        const spy1 = jest.spyOn(h1, "handle");
+        const spy2 = jest.spyOn(h2, "handle");
+
+        const events = { ["CustomerCreatedEvent"]: [h1, h2] };
+
+        const customer = newCustomer(events);
+
+        expect(spy1).toBeCalled();
+        expect(spy2).toBeCalled();
+    });
+
+    it("Should trigger event when changed customer address", () => {
+        const h1 = new SendConsoleLogWhenCustomerAddressIsChangedHandler();
+        const spy1 = jest.spyOn(h1, "handle");
+
+        const events = { ["CustomerAddressChangedEvent"]: [h1] };
+
+        const customer = newCustomer(events);
+
+        customer.changeAddress(new Address(
+            "Street 2", 528, "12345000", "City 2"
+        ));
+
+        expect(spy1).toBeCalled();
     });
 });
