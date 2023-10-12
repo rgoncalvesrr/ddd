01843e8cde94332758516eda4d48c96a4f3c7059
Ricardo Gonçalves <ricardo@conceitho.com>
2023-10-12 19:05:17 -0300

    feat: adiciona lista de eventos de domínio ao construtor da classe

--- src/infrastructure/@shared/repository/repository.utils.ts
+++ src/infrastructure/@shared/repository/repository.utils.ts
@@ -1,9 +1,10 @@
 import { Address } from '../../../domain/customer/value-object/address';
 import { Customer } from '../../../domain/customer/entity/customer';
+import { IEventHandler } from '../../../domain/@shared/event/event-handler.interface';
 import { v4 as uuid } from 'uuid';
 
-export function newCustomer(): Customer {
-    const customer = new Customer(uuid(), "Customer 1");
+export function newCustomer(events: { [eventName: string]: IEventHandler[] } = {}): Customer {
+    const customer = new Customer(uuid(), "Customer 1", events);
     const address = new Address("Rua 1", 123, "09195410", "São Paulo");
 
     customer.changeAddress(address);
