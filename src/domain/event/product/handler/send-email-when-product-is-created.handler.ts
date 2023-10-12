b67207cf01c58140748ca4af9c93f40ad980b944
Ricardo Gonçalves <ricardo@conceitho.com>
2023-10-12 15:11:19 -0300

    feat: adiciona evento ao produto

---
+++ src/domain/event/product/handler/send-email-when-product-is-created.handler.ts
@@ -0,0 +1,9 @@
+import { IEventHandler } from "../../@shared/event-handler.interface";
+import { ProductCreatedEvent } from "../product-created.event";
+
+export class SendEmailWhenProductIsCreateHandler implements IEventHandler<ProductCreatedEvent> {
+    handle(event: ProductCreatedEvent): void {
+        console.log(`Sending e-mail to ${JSON.stringify(event.data)}`);
+    }
+
+}
